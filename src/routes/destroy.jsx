ca7cae34496838d2eceac6c4e6943601d447b407
dullat <dullat@users.noreply.github.com>
2024-07-05 00:03:54 +0530

    update

---
+++ src/routes/destroy.jsx
@@ -0,0 +1,13 @@
+import { redirect } from "react-router-dom"
+import { deleteContact } from "../contacts"
+
+export async function action({ params }) {
+  await deleteContact(params.contactId)
+  return redirect("/")
+}
+
+const destroy = () => {
+  return <div></div>
+}
+
+export default destroy
